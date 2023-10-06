1c1ccf58c4df8e34aac48d8e342f8d59c3e71fc9
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-10-07 00:07:52 +0600

    some folder location is change

--- script/serviceCard.js
+++ script/serviceCard.js
@@ -3,32 +3,32 @@ document.addEventListener('DOMContentLoaded', function () {
     // Service JSON data
     const serviceData = [
         {id: 1, 
-            imgUrl:'images/carpet-cleaning-service.jpg',
-            more:'services/carpetcleaning.html',
+            imgUrl:'../images/carpet-cleaning-service.jpg',
+            more:'carpetcleaning.html',
             title: 'Carpet Cleaning Service in San Diego, CA', 
             content: 'A well-maintained carpet can transform the ambiance of your home, making it warm, inviting, and comfortable. In San Diego, California, where sunny days and outdoor activities are abundant, keeping your carpets clean is essential to maintain a healthy indoor environment. Thankfully, professional carpet cleaning services in San Diego can help you achieve that pristine look and feel for your carpets. In this article, we will explore the importance of carpet cleaning, the benefits of professional services, and how you can find the best carpet cleaning service in San Diego.' 
         },
         {id: 2, 
             imgUrl:"images/carpet-cleaning-services.jpg",
-            more:'services/sofa.html',
+            more:'sofa.html',
             title: 'Service Title 2', 
             content: 'Service a for Card 2' 
         },
         {   id: 3, 
             imgUrl:"images/carpet-cleaning.jpg" ,
-            more:'services/stairs.html',
+            more:'stairs.html',
             title: 'Service Title 3', 
             content: 'Service a for Card Stairs'
         },
         {   id: 4, 
             imgUrl:"images/carpet-cleaning.jpg" ,
-            more:'services/areaRugCleaning.html',
+            more:'areaRugCleaning.html',
             title: 'Service Title area rug cleaning', 
             content: 'Service a for Area rug cleaning'
         },
         {   id: 5, 
             imgUrl:"images/carpet-cleaning.jpg" ,
-            more:'services/upholstery.html',
+            more:'upholstery.html',
             title: 'Service Title  Upholstery cleaning', 
             content: 'Service a for Uphosltery cleaning'
         }
